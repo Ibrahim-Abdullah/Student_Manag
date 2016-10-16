04ff787c4d73ec29555997e8019fbad89529e828
Ibrahim-Abdullah <braimahabdulla@gmail.com>
2016-10-16 00:07:37 -0700

    Getter and Setter method for student.java included

--- src/studentdatabase/Model/Student.java
+++ src/studentdatabase/Model/Student.java
@@ -10,13 +10,65 @@ package studentdatabase.Model;
  * @author derry
  */
 public class Student {
- String studentID;
+    String studentID;
     String surname;
     String firstName;
     int admissionYear;
     float GPA;
     Major program;
     
+    public Student(){
+    this.studentID = null;
+    this.surname = null;
+    this.firstName = null;
+    }
+    public String getSurname(){
+        return this.surname;
+    }
+    
+    public void setSurname(String sureName){
+        this.surname = sureName;
+    }
+    
+    public String getStudentID(){
+        return this.studentID;
+    }
+    
+    public void setStudentID(String studentID){
+        this.studentID = studentID;
+    }
+    public String getFirstname(){
+        return this.firstName;
+    }
+    
+    public void setFirstname(String firstName){
+        this.firstName = firstName;
+    }
+    public int getadmissionYear(){
+        return this.admissionYear;
+    }
+    
+    /**
+     *
+     * @param admissionYear
+     */
+    public void setadmissionYear(int admissionYear){
+        this.admissionYear = admissionYear;
+    }
+    public float getGPA(){
+        return this.GPA;
+    }
+    
+    public void setgpa(float gpa){
+        this.GPA = gpa;
+    }
+    public Major getProgram(){
+        return this.program;
+    }
+    
+    public void setProgram(Major program){
+        this.program = program;
+    }
     public String toStrings() {
      String data = studentID + " " + firstName + " " + surname + " " + admissionYear + " " + GPA + " " + program + "\n";
             return data ;
